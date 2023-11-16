24291f08281eb9f3242bae917094801c66653961
Omer Dolev <omerdolev90@gmail.com>
2023-11-16 19:45:52 +0200

    review basic

--- app.ts
+++ app.ts
@@ -1,6 +1,60 @@
-// array of cards
-// card has front and back
-// display first card
-// let the user flip
-// let the user continue to next card
-// when all cards reviewed show done message
+// [x] array of cards
+// [x] card has front and back
+// [x] display first card
+// [x] let the user flip
+// [x] let the user continue to next card
+// [x] when all cards reviewed show done message
+
+type Card = {
+    front: string,
+    back: string
+};
+
+const cards: Card[] = [
+    {
+        front: "Hello",
+        back: "שלום"
+    },
+    {
+        front: "World",
+        back: "עולם"
+    },
+    {
+        front: "Card",
+        back: "קלף"
+    },
+    {
+        front: "Repetition",
+        back: "חזרה"
+    },
+    {
+        front: "Software",
+        back: "תוכנה"
+    }
+];
+
+let currentCardIndex = 0;
+
+function displayCurrentCard() {
+    const currentCard = cards[currentCardIndex];
+
+    document.querySelector("#currentCard .card__front")!.textContent = currentCard.front;
+    document.querySelector("#currentCard .card__back")!.textContent = currentCard.back;
+}
+
+document.querySelector("#btnFlip")?.addEventListener("click", function () {
+    document.querySelector("#currentCard")?.classList.toggle("card--flipped");
+});
+
+document.querySelector("#btnNextCard")?.addEventListener("click", function () {
+    if (currentCardIndex === cards.length - 1) {
+        document.querySelector("#reviewZone")!.innerHTML = "<h2>All done!</h2>";
+        return;
+    }
+
+    currentCardIndex++;
+    displayCurrentCard();
+});
+
+displayCurrentCard();
+
