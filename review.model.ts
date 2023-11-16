8e8c89639d2c27a7a700359c99d444cbcbc5967f
Omer Dolev <53278705+dolevoper@users.noreply.github.com>
2023-11-16 23:19:13 +0000

    implement sm2

--- review.model.ts
+++ review.model.ts
@@ -2,50 +2,73 @@ type Feedback = 0 | 1 | 2 | 3 | 4 | 5;
 
 export type Card = {
     front: string,
-    back: string
-    lastFeedback?: Feedback;
+    back: string,
+    repetitions: number,
+    interval: number,
+    ef: number
 };
 
+const callbacks = [] as Function[];
 const cards: Card[] = [
     {
         front: "Hello",
-        back: "שלום"
+        back: "שלום",
+        repetitions: 0,
+        interval: 1,
+        ef: 2.5
     },
     {
         front: "World",
-        back: "עולם"
+        back: "עולם",
+        repetitions: 0,
+        interval: 1,
+        ef: 2.5
     },
     {
         front: "Card",
-        back: "קלף"
+        back: "קלף",
+        repetitions: 0,
+        interval: 1,
+        ef: 2.5
     },
     {
         front: "Repetition",
-        back: "חזרה"
+        back: "חזרה",
+        repetitions: 0,
+        interval: 1,
+        ef: 2.5
     },
     {
         front: "Software",
-        back: "תוכנה"
+        back: "תוכנה",
+        repetitions: 0,
+        interval: 1,
+        ef: 2.5
     }
 ];
 
-const callbacks = [] as Function[];
-let currentCardIndex = 0;
+let currentCard: Card | undefined = {
+    front: "Father",
+    back: "אבא",
+    repetitions: 0,
+    interval: 1,
+    ef: 2.5
+};
 
 export function getCurrentCard() {
-    return cards[currentCardIndex];
+    if (!currentCard) {
+        throw new Error();
+    }
+
+    return currentCard;
 }
 
 export function isDone() {
-    return currentCardIndex === cards.length;
+    return !currentCard;
 }
 
 export function nextCard() {
-    if (isDone()) {
-        throw new Error("Review is done.");
-    }
-
-    currentCardIndex++;
+    currentCard = cards.shift();
 
     setTimeout(function () {
         callbacks.forEach(function (callback) {
@@ -55,7 +78,28 @@ export function nextCard() {
 }
 
 export function giveFeedback(feedback: Feedback) {
-    getCurrentCard().lastFeedback = feedback;
+    const currentCard = getCurrentCard();
+
+    if (feedback < 3) {
+        currentCard.repetitions = 0;
+        currentCard.interval = 1;
+
+        cards.push(currentCard);
+
+        return;
+    } else {
+        if (currentCard.repetitions === 0) {
+            currentCard.interval = 1;
+        } else if (currentCard.repetitions === 1) {
+            currentCard.interval = 3;
+        } else {
+            currentCard.interval = Math.round(currentCard.interval * currentCard.ef);
+        }
+
+        currentCard.repetitions++;
+    }
+
+    currentCard.ef = calculateEf(currentCard.ef, feedback);
 }
 
 export function parseFeedback(feedback: unknown): Feedback {
@@ -70,4 +114,8 @@ export function parseFeedback(feedback: unknown): Feedback {
 
 export function onUpdate(callback: Function) {
     callbacks.push(callback);
+}
+
+function calculateEf(currentEf: number, feedback: Feedback) {
+    return Math.min(1.3, currentEf + (0.1 - (5 - feedback) * (0.08 + (5 - feedback) * 0.02)));
 }
